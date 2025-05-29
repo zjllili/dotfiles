73eaf29dda5e317a8cda738988bc98203f3da744
nate zhou <gnuunixchad@outlook.com>
2025-05-29 15:36:20 +0800

    Mod(wmenu-color): separate colors to another script
    Introduce `bin//colors.sh` which can be sourced by multiple scripts

---
+++ .local/bin/colors.sh
@@ -0,0 +1,28 @@
+#/usr/bin/sh
+# @author nate zhou
+# @since 2025
+# colorscheme for wmenu-color and i3blocks scripts
+#. $HOME/.local/bin/colors.sh
+
+foreground="#eeeeee"
+background="#000000"
+#<span foreground=\"$separator\">]</span>"
+separator="#888888"
+
+color0="#111111"
+color1="#f92672"
+color2="#8ce10b"
+color3="#fd971f"
+color4="#bbccdd"
+color5="#bf40bf"
+color6="#8ec07c"
+color7="#5f676a"
+
+color8="#333333"
+color9="#b16286"
+color10="#beed5f"
+color11="#e6da00"
+color12="#076678"
+color13="#9e6ffe"
+color14="#427b58"
+color15="#ffffff"
