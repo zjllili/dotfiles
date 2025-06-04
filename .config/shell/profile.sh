537be5f8b2e9fde5660a04f38783a63d14ad0681
nate zhou <gnuunixchad@outlook.com>
2025-06-04 16:19:58 +0800

    Fix(profile.sh): Sway random keystroke freeze

--- .config/shell/profile.sh
+++ .config/shell/profile.sh
@@ -53,7 +53,7 @@ export QT_QPA_PLATFORMTHEME=qt5ct # qt theme
 export QT_STYLE_OVERRIDE=adwaita-dark # qt theme
 export GTK_THEME=Adwaita-dark # for firejail'ed libreoffice theme
 
-#export WLR_DRM_NO_ATOMIC=1 # fix sway terminal freezes
+export WLR_DRM_NO_ATOMIC=1 # fix sway terminal freezes
 export ELECTRON_OZONE_PLATFORM_HINT=wayland # electron, disabled to run in xwayland for fcitx5 support
 #export WLR_NO_HARDWARE_CURSORS=1 # external monitor cursor on nvidia
 
