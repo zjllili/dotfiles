5bb8aa703904e7417ea4934a774312f78daaf934
nate zhou <gnuunixchad@outlook.com>
2025-07-27 21:38:09 +0800

    Fix(qutebrowser): Not opening URL
    Fix qutebrowser not opening URL for `foot` bindings and `lf/rifle`.

--- .config/shell/profile.sh
+++ .config/shell/profile.sh
@@ -17,7 +17,8 @@ export LC_ALL=en_US.UTF-8 # locale
 export EDITOR="/usr/bin/nvim"
 export PAGER="/usr/bin/less"
 export MOZ_USE_XINPUT2=1 # firefox pixel-perfect trackpad scrolling
-[ -x "/usr/bin/qutebrowser" ] && export BROWSER="$HOME/.local/bin/qb" || export BROWSER="/usr/local/bin/firefox"
+export QTWEBENGINE_CHROMIUM_FLAGS='--disable-gpu' # fix qutebrowser crash on wlroots with nvidia
+[ -x "/usr/bin/qutebrowser" ] && export BROWSER="qutebrowser" || export BROWSER="/usr/local/bin/firefox"
 
 export XDG_DOWNLOAD_DIR="$HOME/dls"
 export XDG_DOCUMENTS_DIR="$HOME/doc"
