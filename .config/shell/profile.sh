d62638f51b8f9232aa2487fbfd5e721a17f4e333
zjllili <36061520+zjllili@users.noreply.github.com>
2025-10-09 00:05:04 +0800

    Update profile.sh

--- .config/shell/profile.sh
+++ .config/shell/profile.sh
@@ -63,9 +63,9 @@ export ELECTRON_OZONE_PLATFORM_HINT=wayland # electron, disabled to run in xwayl
 #export WLR_NO_HARDWARE_CURSORS=1 # external monitor cursor on nvidia
 
 # fcitx
-#export GTK_IM_MODULE=wayland
-#export XMODIFIERS=@im=fcitx
-#export QT_IM_MODULE="wayland;fcitx;ibus"
+export GTK_IM_MODULE=wayland
+export XMODIFIERS=@im=fcitx
+export QT_IM_MODULE="wayland;fcitx;ibus"
 
 # ibus
 #export GTK_IM_MODULE=ibus
